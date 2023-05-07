f3a893fbafad876a31bab4d035eb5e6622f0e649
Matthew Cover <41007986+matthewncover@users.noreply.github.com>
2023-05-07 11:11:41 -0700

    add push to snowflake

--- snowflake_utils.py
+++ snowflake_utils.py
@@ -16,6 +16,7 @@ def create_snowflake_connection():
     
     return conn
 
+
 def query_snowflake(query):
 
     conn = create_snowflake_connection()
@@ -26,6 +27,25 @@ def query_snowflake(query):
         result = cursor.fetchall()
         return result
     
+    finally:
+        cursor.close()
+        conn.close()
+
+
+def push_to_snowflake(table_name:str, columns:list, values:list):
+
+    conn = create_snowflake_connection()
+
+    try:
+        cursor = conn.cursor()
+
+        columns_str = ', '.join(columns)
+        values_str = ', '.join(['%s']*len(values))
+        query = f"INSERT INTO {table_name} ({columns_str}) VALUES ({values_str})"
+
+        cursor.execute(query, values)
+        conn.commit()
+
     finally:
         cursor.close()
         conn.close()
