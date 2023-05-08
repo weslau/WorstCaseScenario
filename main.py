24fca5d1c56279ff54e3da8503baceb78767a079
Matthew Cover <41007986+matthewncover@users.noreply.github.com>
2023-05-08 13:49:56 -0700

    add new game codes to db with create

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import streamlit as st
-import pandas as pd
+import pandas as pd, datetime as dt
 
-import random, string
+import random, string, uuid
 
 import snowflake_utils as snow
 
+DB_NAME = "DEV_WORSTCASESCENARIO_DB" 
+SCHEMA_NAME = "WCS_DB_SCHEMA1"
+
 def generate_game_code():
     return ''.join(random.choice(string.ascii_uppercase) for _ in range(6))
+
+def generate_uuid():
+    pass
 
 def header():
     st.write("### Worst Case Scenario")
@@ -21,7 +27,6 @@ def back_widget(to:str="welcome"):
 def welcome_page():
     header()
 
-    # if "current_page" not in st.session_state:
     st.session_state.current_page = "welcome"
 
     create_game_button = st.button("Create Game")
@@ -32,18 +37,31 @@ def welcome_page():
 
         new_game_code = generate_game_code()
         st.session_state.game_code = new_game_code
+        table_columns = ["GAME_ID", "GAME_CODE", "GAME_TIME_START", "IS_ACTIVE"]
+        table_values = [str(uuid.uuid4()), st.session_state.game_code, dt.datetime.now(), True]
+        snow.push("GAMES", table_columns, table_values)
 
         st.session_state.current_page = "lobby"
         st.experimental_rerun()
 
     if join_game_button:
-        st.text_input("Enter 6-digit game code")
+        entered_game_code = st.text_input("Enter 6-digit game code")
+
+        if entered_game_code:
+            query = f"""
+            select * from {DB_NAME}.{SCHEMA_NAME}.GAMES
+            where IS_ACTIVE = TRUE
+            """
+
+            result = snow.pull(query)
+            print(result)
 
 
 def lobby_page():
     header()
 
     st.write(f"### code: {st.session_state.game_code}")
+    
         
     ## show lobby page; how many players have joined and whom
     back_widget()
@@ -51,7 +69,18 @@ def lobby_page():
 
 if __name__ == "__main__":
 
-    if "current_page" not in st.session_state or st.session_state.current_page == "welcome":
+    page_dict = {
+        "welcome": welcome_page,
+        "lobby": lobby_page
+    }
+
+    if "current_page" not in st.session_state:
         welcome_page()
-    elif st.session_state.current_page == "lobby":
-        lobby_page()
+    
+    else:
+        page_dict[st.session_state.current_page]()
+
+    # if "current_page" not in st.session_state or st.session_state.current_page == "welcome":
+    #     welcome_page()
+    # elif st.session_state.current_page == "lobby":
+    #     lobby_page()
