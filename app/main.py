916260be1a32bb3049b9eb250d97adaee8064324
Wesley Lau <wesley.lau@viasat.com>
2023-05-24 22:54:11 -0700

    changed reading in scenarios from rows.txt so that it works in streamlit cloud deployed container

--- app/main.py
+++ app/main.py
@@ -83,7 +83,7 @@ def play_round_page():
     cwd = os.getcwd()
     # Join the directory and file name to create the full file path
     scenarios_file_path = os.path.join(cwd, "rows.txt")
-    print(scenarios_file_path)
+    st.write(scenarios_file_path)
     scenarios_file_path = "/app/worstcasescenario/app/rows.txt"
     data_scenarios = gameplay.read_rows_from_file(file_path=scenarios_file_path)
     def get_random_options():
